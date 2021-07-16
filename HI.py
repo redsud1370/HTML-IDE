b39235df03c9418b5ade6aa47039f81cabad8637
redsud1370 <84220499+redsud1370@users.noreply.github.com>
2021-07-16 19:51:11 +0530

    Add files via upload

---
+++ HI.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import ImageTk, Image
+import os
+
+root = Tk()
+root.minsize(650,650)
+root.maxsize(650,650)
+
+open_img = ImageTk.PhotoImage(Image.open ("open.png"))
+save_img = ImageTk.PhotoImage(Image.open ("save.png"))
+exit_img = ImageTk.PhotoImage(Image.open ("exit.jpg"))
+
+label_file_name = Label(root, text="File name")
+label_file_name.place(relx=0.28,rely=0.03,anchor= CENTER)
+
+input_file_name = Entry(root)
+input_file_name.place(relx=0.46,rely=0.03, anchor= CENTER)
+
+my_text= Text(root,height=35,width=80)
+my_text.place(relx=0.5,rely=0.55,anchor= CENTER)
+
+root.mainloop()
+
+
+
